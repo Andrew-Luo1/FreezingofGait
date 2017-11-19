f5ffffbf91ec83fe96157bd4c73e9c9ccf52cd8f
Andrew-Luo1 <andrew.luo1@outlook.com>
2017-11-19 09:14:21 -0500

    SVM added

---
+++ SVM.m
@@ -0,0 +1,19 @@
+function SVM( X, y, C, max_iter )
+%Graph the SVM, provide coefficients for the decision boundary. Max_iter
+%can be around 20. X is a two column array containing feature values. y is
+%a vector storing the class information. (0 or 1; freezing or normal)
+
+    model = svmTrain(X, y, C, @linearKernel, 1e-3, max_iter);
+    w = model.w;
+    b = model.b;
+    xp = linspace(min(X(:,1)), max(X(:,1)), 100);
+    yp = - (w(1)*xp + b)/w(2);
+    plotData(X, y);
+    hold on;
+    plot(xp, yp, '-b'); 
+    hold off
+    model.w
+    model.b
+
+end
+
